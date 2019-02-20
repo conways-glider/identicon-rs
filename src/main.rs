3dbb19d09400c143c08f8adf9cf090e1e985ceff
Nia Watts <niawatts95@gmail.com>
2019-02-20 00:29:55 -0700

    updated main.rs example file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use identicon_rs;
 
 fn main() {
-    identicon_rs::save_scaled_image("fluffy-samurai", "output_1.png", 500);
-    identicon_rs::save_scaled_image("identicon", "output_2.png", 500);
+    let fluffy_samurai = "fluffy-samurai";
+    let test_string = "identicon_rs";
+    identicon_rs::save_scaled_image(fluffy_samurai, "output_1.png", 500);
+    identicon_rs::save_scaled_image(test_string, "output_2.png", 500);
 
-    identicon_rs::save_bordered_image("fluffy-samurai", "output_1_bordered.png", 500, 50);
-    identicon_rs::save_bordered_image("identicon", "output_2_bordered.png", 500, 50);
+    identicon_rs::save_bordered_image(fluffy_samurai, "output_1_bordered.png", 500, 50);
+    identicon_rs::save_bordered_image(test_string, "output_2_bordered.png", 500, 50);
 }
