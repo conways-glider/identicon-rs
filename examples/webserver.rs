13c68b65e6fe476a89611feff08374f703a8b806
Nia Watts <niawatts95@gmail.com>
2019-10-05 14:11:06 -0600

    Update dependencies

--- examples/webserver.rs
+++ examples/webserver.rs
@@ -1,17 +1,17 @@
-use actix_web::{server, App, HttpRequest, HttpResponse};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use identicon_rs::Identicon;
 
-fn index(req: &HttpRequest) -> HttpResponse {
-    let input_string: String = req.match_info().query("input_string").unwrap();
-    let identicon_fluffy = Identicon::new_default(&input_string);
+fn index(path_input: web::Path<String>) -> impl Responder {
+    let identicon_fluffy = Identicon::new_default(&path_input);
     let file = identicon_fluffy.export_file_data();
 
     HttpResponse::Ok().content_type("image/png").body(file)
 }
 
 fn main() {
-    server::new(|| App::new().resource("/{input_string}", |r| r.f(index)))
+    HttpServer::new(|| App::new().route("/{input_string}", web::get().to(index)))
         .bind("127.0.0.1:8088")
         .unwrap()
-        .run();
+        .run()
+        .unwrap();
 }
