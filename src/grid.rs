e614e113ab22d909af17d82a35c121ce551108c1
Stefan Schindler <dns2utf8@estada.ch>
2020-08-29 13:16:53 +0200

    Shorten code

--- src/grid.rs
+++ src/grid.rs
@@ -1,8 +1,7 @@
 pub fn generate_full_grid<T: Into<u32>>(image_size: T, hash: &[u8]) -> Vec<bool> {
     // Compute the hash value
     let square_count = (image_size.into() as usize).pow(2);
-    let grid: Vec<bool> = (0..square_count)
+    (0..square_count)
         .map(|location| hash[location % hash.len()] % 2 == 0)
-        .collect();
-    grid
+        .collect()
 }
