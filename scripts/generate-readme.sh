1571d61d795146af9b251029e5397310e82f7a69
Nia Watts <niawatts95@gmail.com>
2022-08-14 12:57:42 -0600

    Breakup git and sed commands

--- scripts/generate-readme.sh
+++ scripts/generate-readme.sh
@@ -3,7 +3,10 @@
 set -euo pipefail
 set -x
 
-VERSION=$(git describe --tags --abbrev=0 | sed 's/^v//')
+TAG=$(git describe --tags --abbrev=0)
+echo $TAG
+
+VERSION=$(echo $TAG | sed 's/^v//')
 echo $VERSION
 
 sed "s/{VERSION}/${VERSION}/g" ./scripts/README_TEMPLATE.md > README.md
