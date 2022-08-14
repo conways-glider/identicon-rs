9209fcb8f79664775d9083578c51b28c871124bb
Nia Watts <niawatts95@gmail.com>
2022-08-14 12:56:14 -0600

    Fix tags and print commands

--- scripts/generate-readme.sh
+++ scripts/generate-readme.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 set -euo pipefail
+set -x
 
-VERSION=$(git describe --tags | sed 's/^v//')
+VERSION=$(git describe --tags --abbrev=0 | sed 's/^v//')
 echo $VERSION
 
 sed "s/{VERSION}/${VERSION}/g" ./scripts/README_TEMPLATE.md > README.md
