8e870e832a4324b06cca71e0344ca1c4d1c748db
Nia Watts <niawatts95@gmail.com>
2022-11-06 20:36:18 -0700

    Revert "Remove version number test"
    This reverts commit 96bfcca0cc86e258abd63380605db0a491b8f747.

--- tests/version-numbers.rs
+++ tests/version-numbers.rs
@@ -4,6 +4,12 @@ fn test_readme_deps_updated() {
 }
 
 #[test]
+fn test_readme_dependencies_badge_version() {
+    let template = "[![dependency status](https://deps.rs/crate/identicon-rs/{version}/status.svg)](https://deps.rs/crate/identicon-rs/{version})";
+    version_sync::assert_contains_substring!("README.md", template);
+}
+
+#[test]
 fn test_html_root_url() {
     version_sync::assert_html_root_url_updated!("src/lib.rs");
 }
