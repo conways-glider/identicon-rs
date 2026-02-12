cb75615331ad233edb4f052c961bec6256c12a02
Nia <nia@conwaysglider.com>
2026-02-11 23:42:15 -0700

    Use updated function

--- src/grid.rs
+++ src/grid.rs
@@ -5,6 +5,6 @@ pub fn generate_full_grid<T: Into<u32>>(image_size: T, hash: &[u8]) -> Vec<bool>
     // Compute the hash value
     let square_count = (image_size.into() as usize).pow(2);
     (0..square_count)
-        .map(|location| hash[location % hash.len()] % 2 == 0)
+        .map(|location| hash[location % hash.len()].is_multiple_of(2))
         .collect()
 }
